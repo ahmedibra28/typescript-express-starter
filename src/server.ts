96a32ca360e9be5b237f0ecb3c8148d549554d75
Ahmed <ahmaat19@gmail.com>
2025-03-12 11:38:38 +0300

    refactor: change eslintrc js to json and remove logger log from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,22 @@
-import express, { Request, Response } from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
-import helmet from 'helmet'
-import logger from './config/logger'
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import helmet from 'helmet';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(express.json())
-const PORT = process.env.PORT || 5000
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(helmet())
-app.use(express.json())
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello from Node.js + TypeScript!' })
-})
+  res.json({ message: 'Hello from Node.js + TypeScript!' });
+});
 
 app.listen(PORT, () => {
-  logger.info(`🚀 Server running on http://localhost:${PORT}`)
-})
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
